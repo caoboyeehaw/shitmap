a31134941eb9acbf2029ad05ba032c69b97c6168
caoboyeehaw <dylanhoangcao@gmail.com>
2023-08-02 18:20:40 -0500

    initial commit for this repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,254 @@
+'use client';
+
 import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import React, { useEffect, useState, useRef } from 'react';
+import * as THREE from 'three';
+import WaveSurfer from 'wavesurfer.js';
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+class SimpleEqualizer {
+  private context: AudioContext;
+  private source: AudioBufferSourceNode | null;
+  private filter: BiquadFilterNode;
+  private gain: GainNode;
+  private audioFile: File;
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  setFilterType(type: BiquadFilterType) {
+    this.filter.type = type;
+  }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  getAudioFile(): File {
+    return this.audioFile;
+  }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+  constructor(audioFile: File) {
+    this.context = new AudioContext();
+    this.source = null;
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    this.filter = this.context.createBiquadFilter();
+    this.gain = this.context.createGain();
+    this.audioFile = audioFile;
+
+    // Configure the filter
+    this.filter.type = 'bandpass';
+    this.filter.frequency.value = 1000; // Center frequency (in Hz)
+    this.filter.Q.value = 1; // Quality factor
+
+    // Connect everything
+    this.filter.connect(this.gain);
+    this.gain.connect(this.context.destination);
+  }
+
+  setFrequency(frequency: number) {
+    this.filter.frequency.value = frequency;
+  }
+
+  setGain(gain: number) {
+    this.gain.gain.value = gain;
+  }
+
+  play() {
+    // Load the audio file into the source
+    const reader = new FileReader();
+    reader.onload = (event: ProgressEvent<FileReader>) => {
+      if (!event.target?.result) return;
+    
+      this.context.decodeAudioData(event.target.result as ArrayBuffer)
+        .then(buffer => {
+          this.source = this.context.createBufferSource();
+          this.source.buffer = buffer;
+          this.source.connect(this.filter);
+          this.source.start();
+        })
+        .catch(error => console.error('Error decoding audio data', error));
+    };
+
+    reader.readAsArrayBuffer(this.audioFile);
+  }
+
+  stop() {
+    if (this.source) {
+      this.source.stop();
+      this.source = null;
+    } else {
+      console.error('No source to stop');
+    }
+  }
+
 }
+
+
+export default function Home() {
+  const [equalizer, setEqualizer] = useState<SimpleEqualizer | null>(null);
+  const waveformRef = useRef<HTMLDivElement | null>(null);
+  
+
+  useEffect(() => {
+    if (equalizer) {
+      // Cast the WaveSurfer instance to any to suppress type checking
+      const wavesurfer = WaveSurfer.create({
+        container: waveformRef.current as HTMLDivElement | string,
+        waveColor: 'violet',
+        progressColor: 'purple',
+        backend: 'MediaElement'
+      }) as any;  // Cast to any
+  
+      wavesurfer.loadBlob(equalizer.getAudioFile());
+  
+      wavesurfer.on('ready', function () {
+        console.log('wavesurfer is ready');
+        console.log(wavesurfer);
+        if ((wavesurfer as any).backend) { // No TypeScript error
+          console.log('wavesurfer.backend is defined');
+          console.log((wavesurfer as any).backend);
+          if (typeof (wavesurfer as any).backend.getPeaks === 'function') {
+            console.log('wavesurfer.backend.getPeaks is a function');
+          } else {
+            console.error('wavesurfer.backend.getPeaks is not a function');
+          }
+        } else {
+          console.error('wavesurfer.backend is undefined');
+        }
+        try {
+          const waveform = (wavesurfer as any).backend.getPeaks(255);
+
+      
+          // Create an array to store the vertices
+          const vertices = [];
+        
+          for (let i = 0; i < waveform.length; i++) {
+            const value = waveform[i];
+            // Instead of pushing to geometry.vertices, push to the vertices array
+            vertices.push(i, value * 50, 0);
+          }
+            // Create a Float32Array from the vertices array
+          const verticesArray = new Float32Array(vertices);
+
+          // Create a BufferAttribute with the verticesArray
+          const bufferAttribute = new THREE.BufferAttribute(verticesArray, 3);
+
+          // Create a BufferGeometry
+          const geometry = new THREE.BufferGeometry();
+
+          // Add the BufferAttribute to the BufferGeometry
+          geometry.setAttribute('position', bufferAttribute);
+    
+          const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+          const line = new THREE.Line(geometry, material);
+    
+          // Create a scene, camera, and renderer
+          const scene = new THREE.Scene();
+          const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+          camera.position.z = 5;
+    
+          const renderer = new THREE.WebGLRenderer();
+          renderer.setSize(window.innerWidth, window.innerHeight);
+          document.body.appendChild(renderer.domElement);
+    
+          // Add the line to the scene
+          scene.add(line);
+    
+          // Render the scene
+          const animate = function () {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+          };
+    
+          animate();
+        } catch (error) {
+          console.error('Error getting peaks:', error);
+        }
+
+      });
+    }
+  }, [equalizer]);
+
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      setEqualizer(new SimpleEqualizer(file));
+    }
+  };
+
+  const handleFilterTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    if (equalizer) {
+      equalizer.setFilterType(event.target.value as BiquadFilterType);
+    }
+  };
+
+  const handlePlay = () => {
+    if (equalizer) {
+      equalizer.play();
+    }
+  };
+
+  const handleStop = () => {
+    if (equalizer) {
+      equalizer.stop();
+    }
+  };
+
+  const handleFrequencyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (equalizer) {
+      equalizer.setFrequency(parseFloat(event.target.value));
+    }
+  };
+  
+  const handleGainChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (equalizer) {
+      equalizer.setGain(parseFloat(event.target.value));
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-5 space-y-4">
+      <div className="text-2xl font-bold">SHITMAP PRE ALPHA </div>
+      <input type="file" onChange={handleFileChange} className="mb-4" />
+      <div className="flex justify-between space-x-4">
+        <button 
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={handlePlay}
+        >
+          Play
+        </button>
+        <button 
+          className="bg-red-500 text-white px-4 py-2 rounded"
+          onClick={handleStop}
+        >
+          Stop
+        </button>   
+      </div>
+
+      <div className="space-y-2">
+        <label className="block">Filter Type:</label>
+        <select onChange={handleFilterTypeChange} className="w-full p-2">
+          <option value="lowpass">Lowpass</option>
+          <option value="highpass">Highpass</option>
+          <option value="bandpass">Bandpass</option>
+          <option value="lowshelf">Lowshelf</option>
+          <option value="highshelf">Highshelf</option>
+          <option value="peaking">Peaking</option>
+          <option value="notch">Notch</option>
+          <option value="allpass">Allpass</option>
+        </select>
+      </div>
+
+      <div className="space-y-2">
+        <label className="block">Frequency:</label>
+        <input type="range" min="20" max="20000" step="1" onChange={handleFrequencyChange} className="w-full" />
+      </div>
+
+      <div className="space-y-2">
+        <label className="block">Gain:</label>
+        <input type="range" min="0" max="1" step="0.01" onChange={handleGainChange} className="w-full" />
+      </div>
+      
+      <div ref={waveformRef} />
+
+
+    </div>
+  );
+
+}
+
